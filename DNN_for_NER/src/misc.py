7ff72615b1d54f8ce51c0884e3ce4f8da33b84c3
machilusZ <yunanzhang7715@gmail.com>
2017-08-08 16:12:25 -0700

    NLP projects

---
+++ DNN_for_NER/src/misc.py
@@ -0,0 +1,16 @@
+##
+# Helper method
+##
+
+import numpy as np
+
+def random_weight_matrix(m, n):
+    #### YOUR CODE HERE ####
+
+
+    epsilon = np.sqrt(6)/np.sqrt(m+n)
+    A0 = (np.random.random((m,n)) - 0.5) * epsilon * 2
+    #### END YOUR CODE ####
+    assert(A0.shape == (m,n))
+    return A0
+
